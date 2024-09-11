c372bfc4f9c53b299dd5ba11571b3278043953b7
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-09-11 09:04:28 +0200

    setup of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { MainContentComponent } from './component/main-content/main-content.component';
+import { HoverHighlightDirective } from './hover-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContentComponent,
+    HoverHighlightDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hover-highlight.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appHoverHighlight]'
+})
+export class HoverHighlightDirective {
+
+  constructor() { }
+
+}

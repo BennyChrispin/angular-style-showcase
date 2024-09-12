4e6e95f9a33b641aa524bbecefba83e2f8c190da
BennyChrispin <benny.ndizihiwe@amalitechtraining.org>
2024-09-12 10:04:09 +0200

    done about component and Contact component and fix responsiveness

--- src/app/component/contanct/contanct.component.ts
+++ src/app/component/contanct/contanct.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contanct',
   templateUrl: './contanct.component.html',
-  styleUrl: './contanct.component.scss'
+  styleUrl: './contanct.component.scss',
 })
 export class ContanctComponent {
-
+  onSubmit() {
+    console.log('Form submitted');
+  }
 }
